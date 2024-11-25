f95ccb830bbad6ce1d3dc5b030ba78818e618ee8
jwkim <wer07032@naver.com>
2024-11-25 11:10:33 +0900

    rollback next.config.ts

--- Next.js/getting-started/nextjs-dashboard/next.config.ts
+++ Next.js/getting-started/nextjs-dashboard/next.config.ts
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
+import type { NextConfig } from 'next';
 
-const nextConfig = {
-  experimental: {
-    ppr: 'incremental',
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
