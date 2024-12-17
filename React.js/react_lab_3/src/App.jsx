4f818cd9f70becf96604649e6081b8319a923c02
jwkim <wer07032@naver.com>
2024-12-17 13:19:33 +0900

    React Udemy #4:Review What I Learned

--- React.js/react_lab_3/src/App.jsx
+++ React.js/react_lab_3/src/App.jsx
@@ -1,9 +1,27 @@
 import UserInput from "./components/UserInput";
+import Result from "./components/Result";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput(prevUserInput => {
+        return{
+            ...prevUserInput,
+            [inputIdentifier]: +newValue
+        };
+    });
+}
 
   return <main>
-    <UserInput />
+    <UserInput onChange={handleChange} userInput={userInput}/>
+    <Result input={userInput}/>
   </main>;
 }
 
